d9dd6587db6338ae812f65bc226dde5921239535
Andrew taylor <andrewt@cse.usnw.edu.au>
2017-10-16 15:56:37 +1100

    make datasets public for tut11 questions

--- tlb/09/solutions/distinct_lines.v1.py
+++ tlb/09/solutions/distinct_lines.v1.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-# written by evank@cse.unsw.edu.au
 
-import sys
-import re
-n = int(sys.argv[1])
-seen = set()
-for i, line in enumerate(sys.stdin, start=1):
-    seen.add(re.sub(r"\s", "", line.lower())
-    if len(seen) == n:
-        print("{} distinct lines seen after {} lines read.".format(len(seen), i)
-        break
-else:
-    print("End of input reached after {} lines read - {} different lines not seen.".format(i, n))
+import re, sys
+
+n_distinct_lines_needed = int(sys.argv[1])
+
+n_lines = 0
+lines_seen = {}
+while 1:
+	line = sys.stdin.readline()
+	if not line:
+		print("End of input reached after", n_lines, 'lines read - ', n_distinct_lines_needed, 'different lines not seen.')
+		sys.exit(0)
+	n_lines += 1
+	line = line.lower()
+	line = re.sub(r'\s', '', line)
+	lines_seen[line] = 1
+	if len(lines_seen.keys()) >= n_distinct_lines_needed:
+		print(n_distinct_lines_needed, 'distinct lines seen after', n_lines, 'lines read.')
+		sys.exit(0)
+
